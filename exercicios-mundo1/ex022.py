31404db9f28cb21883f3fdd2f39e809e140c894c
Guilherme Alves <guialvescarvalh001@gmail.com>
2026-07-26 20:28:19 -0300

    Feita a resolução do exercicio 022

--- exercicios-mundo1/ex022.py
+++ exercicios-mundo1/ex022.py
@@ -1,1 +1,13 @@
-print('olá mundo')
+#resolução exercicio 022 - Curso em Video
+
+nome = input('digite seu nome: ')
+
+print(f'{nome.upper()} letras maiusculas e {nome.lower()} letras minuscula')
+
+primeiro = nome.split()
+print(f'O primeiro nome tem {len(primeiro[0])} letras')
+
+tamanho = len(primeiro[0]) + len(primeiro[1]) + len(primeiro[2])
+print(f'O nome tem {tamanho} letras')
+
+
