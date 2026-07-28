f0acd9dad414e936cdcf6c78dfbc74a97009fe87
Guilherme Alves <guialvescarvalh001@gmail.com>
2026-07-28 20:09:41 -0300

    Adicionando resolução do exercicio 027

---
+++ exercicios-mundo1/ex027.py
@@ -0,0 +1,6 @@
+#resolução do exercicio 025 - Curso em video
+n = input('Digite seu nome:')
+nome = n.split()
+print(f'seu primeiro nome é {nome[0]}')
+print(f'seu ultimo nome é {nome[len(nome) - 1]}')
+
