b89159908a49dc05a01e51323da51748f4fe97a8
Guilherme Alves <guialvescarvalh001@gmail.com>
2026-08-04 14:22:44 -0300

    Resolução do exercicio 061

---
+++ exercicios-mundo2/ex061.py
@@ -0,0 +1,10 @@
+#Resolução de exercicios 061 - Curso em Video
+termo = int(input('Digite o primeiro termo: '))
+razao = int(input('Digite a razão da PA: '))
+
+i = 0 
+
+while i != 10:
+    print(termo)
+    termo += razao 
+    i += 1
