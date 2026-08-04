f842e07c07b9951a7fe50333be0dd3a4256e0984
Guilherme Alves <guialvescarvalh001@gmail.com>
2026-08-03 23:37:35 -0300

    Solução exercicio 057

---
+++ exercicios-mundo2/ex057.py
@@ -0,0 +1,8 @@
+#Resolução do exercicio 057 - Curso em Video
+
+sexo = input('Digite seu sexo, M ou F? ').upper()
+
+while sexo not in "FM":
+    sexo = input('Digite seu sexo, M ou F? ').upper()
+
+print('Fim, você digitou corretamente')
