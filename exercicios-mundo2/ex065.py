16b5a7b6c75f54cd3640673b04793797c0682556
Guilherme Alves <guialvescarvalh001@gmail.com>
2026-08-06 19:44:35 -0300

    Resolução exercicio 065

---
+++ exercicios-mundo2/ex065.py
@@ -0,0 +1,24 @@
+#Resolução exercicio 065 - Curso em video
+
+i = ''
+cont = num = maior = menor = media = 0
+
+while i != 'N':
+    num = int(input('Digite um numero: '))
+    x = num
+
+    if num > maior:
+        maior = num
+    
+    if cont == 0:
+        menor = num
+    else:
+        if menor > num:
+            menor = num
+
+
+    i = str(input('Quer continuar? [S/N]: ')).upper()
+    cont += 1
+    media += num
+
+print(f'O menor numero digitado foi {menor}, o maior foi {maior}, e a media dos numeros é {media / cont}')
